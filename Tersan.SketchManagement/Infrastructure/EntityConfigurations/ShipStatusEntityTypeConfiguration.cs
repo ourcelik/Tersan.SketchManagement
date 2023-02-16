f293d660f3f165aec282e6b8b92ae174600d202f
Onur Çelik <180101013@ogrenci.yalova.edu.tr>
2023-02-16 11:22:04 +0300

    seeding StatusType TR

--- Tersan.SketchManagement/Infrastructure/EntityConfigurations/ShipStatusEntityTypeConfiguration.cs
+++ Tersan.SketchManagement/Infrastructure/EntityConfigurations/ShipStatusEntityTypeConfiguration.cs
@@ -6,6 +6,9 @@ namespace Tersan.SketchManagement.Infrastructure.EntityConfigurations
 {
     public class ShipStatusEntityTypeConfiguration : IEntityTypeConfiguration<ShipStatus>
     {
+        const string CONST_MAINTENANCE_TR = "Bakımda";
+        const string CONST_IN_PRODUCTION_TR = "Üretimde";
+
         public void Configure(EntityTypeBuilder<ShipStatus> builder)
         {
             builder.ToTable("ShipStatuses");
@@ -15,6 +18,14 @@ namespace Tersan.SketchManagement.Infrastructure.EntityConfigurations
             builder.Property("StatusType")
                 .IsRequired()
                 .HasMaxLength(50);
+
+            //Seed data
+
+            builder.HasData(
+                new ShipStatus { ID = 1, StatusType = CONST_MAINTENANCE_TR },
+                new ShipStatus { ID = 2, StatusType = CONST_IN_PRODUCTION_TR }
+            );
+
         }
     }
     
---
+++ Tersan.SketchManagement/Migrations/20230216081833_Ship_Status_Seed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Tersan.SketchManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShipStatusSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ShipStatuses",
+                columns: new[] { "ID", "StatusType" },
+                values: new object[,]
+                {
+                    { 1, "Bakımda" },
+                    { 2, "Üretimde" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ShipStatuses",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ShipStatuses",
+                keyColumn: "ID",
+                keyValue: 2);
+        }
+    }
+}
